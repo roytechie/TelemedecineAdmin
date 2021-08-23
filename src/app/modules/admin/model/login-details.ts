79a61345c1365333beef672a801b6247f1863599
Raja Ghosh <work.rajaghosh@gmail.com>
2021-08-23 18:53:00 +0530

    Reverted Changes

--- src/app/modules/admin/model/login-details.ts
+++ src/app/modules/admin/model/login-details.ts
@@ -2,39 +2,38 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class LoginDetails {
-  FirstName: string
-  LastName: string
-  UserName: string
-  Password: string
-  Email: string
-  UserId: number
-  isActiveUser: boolean
-  AdminUserId: number
-  AdminUserName: string
-  PhoneNumber: string
+    FirstName: string
+    LastName: string
+    UserName: string
+    Password: string
+    Email: string
+    UserId: number
+    isActiveUser: boolean
+    AdminUserId: number
+    AdminUserName: string
+    PhoneNumber: string
 }
 
 export enum AccessLavel {
-  Admin = 1,
-  Pharmacy = 9,
-  Other = 10
+    Admin = 1,
+    Pharmacy = 9,
+    Other = 10
 }
 
 @Injectable()
 export class ReportRequest {
-  startDate: string
-  endDate: string
-  isAdmin: boolean
-  userId: number
-  submissionId: number
-  isSingleSubmission: boolean = false;
-  reportType: string
-  symptomList: string
+    startDate: string
+    endDate: string
+    isAdmin: boolean
+    userId : number
+    submissionId : number
+    isSingleSubmission: boolean = false;
+    reportType : string
 }
 
 export class AuthProperties {
-  IsVerifiedUser: boolean
-  loginDetails: LoginDetails
+    IsVerifiedUser : boolean
+    loginDetails: LoginDetails
 }
 
 
