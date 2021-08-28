0c6fa81673fa8857dd6887c900130ffddee9555f
Raja Ghosh <work.rajaghosh@gmail.com>
2021-08-28 23:49:08 +0530

    Changes for Mail Send Update

--- src/app/modules/admin/components/sendmail-patient/sendmail-patient.component.ts
+++ src/app/modules/admin/components/sendmail-patient/sendmail-patient.component.ts
@@ -9,6 +9,7 @@ import { AdminService } from '../../service/admin.service';
 import { AthenticationService } from '../../service/athentication.service';
 import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-sendmail-patient',
@@ -18,6 +19,15 @@ import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.compone
 export class SendmailPatientComponent implements OnInit {
 
   emailprop: EmailProperties;
+
+  startDate: Date = new Date();
+  endDate : Date = new Date();
+
+  symptomsFilter: string;
+  statusFilter: any = '-1';
+
+  dataSource = new MatTableDataSource<any>(); 
+  dataSourceOriginal = new MatTableDataSource<any>();
 
   emailForm = new FormGroup({
     subject: new FormControl('', [Validators.required]),
@@ -88,6 +98,143 @@ export class SendmailPatientComponent implements OnInit {
     });
   }
 
+
+  updateFilter(event: any, filterType: string) 
+  { 
+
+    // if(filterType == 'symptomsFilter')
+    // {
+    //   if(event.value == 'All'){
+    //     event.value = 'All,Covid,NonCovid';
+    //   }
+    //   if(event.value == 'Covid,NonCovid'){
+    //     event.value = 'All,Covid,NonCovid';
+    //   }
+    //   this.symptomsFilter = event.value;
+    // }
+    // else
+    // {
+    //   this.statusFilter = event.value;
+    // }
+
+    let statusString; 
+
+    if(this.statusFilter == '-1')
+    {
+      statusString = 'NoStatus';
+    }
+    else if(this.statusFilter == 7){
+      statusString = 'Complete';
+    }
+    else
+    {
+      statusString = 'Status';
+    }
+
+    let searchString = this.symptomsFilter + 'with' + statusString;
+    this.getFilterdResult(searchString);
+  } 
+
+  getFilterdResult(searchString: string){
+    let status = parseInt(this.statusFilter);
+    let completeStatusId = 7;
+
+    switch(searchString) 
+    {
+      case 'AllwithNoStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status != completeStatusId);
+        });
+        break;
+
+      case 'AllwithStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status != completeStatusId && item.status == status);
+        });
+        break;
+
+      case 'AllwithComplete':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status == completeStatusId);
+        });
+        break;
+
+      case 'CovidwithNoStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.isCovid == true && item.status != completeStatusId);
+        });
+        break;
+
+      case 'CovidwithStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.isCovid == true && item.status != completeStatusId && item.status == status);
+        });
+        break;
+
+      case 'CovidwithComplete':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.isCovid == true && item.status == completeStatusId );
+        });
+        break;
+
+      case 'NonCovidwithNoStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.isCovid == false && item.status != completeStatusId );
+        });
+        break;
+
+      case 'NonCovidwithStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.isCovid == false && item.status != completeStatusId && item.status == status);
+        });
+        break;
+
+      case 'NonCovidwithComplete':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.isCovid == false && item.status == completeStatusId );
+        });
+        break;     
+        
+      //This will be the cases when Both Covid And Non Covid Patients are evaluated
+      case 'All,Covid,NonCovidwithNoStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status != completeStatusId );
+        });
+        break;
+
+      case 'All,Covid,NonCovidwithStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status != completeStatusId && item.status == status);
+        });
+        break;
+
+      case 'All,Covid,NonCovidwithComplete':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status == completeStatusId );
+        });
+        break;
+      
+      //This will be the cases when neither Covid nor Non Covid Patients are evaluated
+      case 'withNoStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status != completeStatusId );
+        });
+        break;
+
+      case 'withStatus':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status != completeStatusId && item.status == status);
+        });
+        break;
+
+      case 'withComplete':  
+        this.dataSource.data = this.dataSourceOriginal.filteredData.filter(function(item) { 
+          return (item.status == completeStatusId );
+        });
+        break;
+    }
+  }
+
   sendMailWithConfirmation() {
     if (this.emailForm.invalid) {
       console.log(this.emailForm.value['body']);
@@ -96,7 +243,7 @@ export class SendmailPatientComponent implements OnInit {
      else{
       const dialogRef = this.dialog.open(ConfirmDialogComponent, {
         data: {
-          message: 'Are you sure want to send email to all patients?',
+          message: 'Are you sure want to send email to selected patients?',
           buttonText: {
             ok: 'Send',
             cancel: 'No'
@@ -110,11 +257,21 @@ export class SendmailPatientComponent implements OnInit {
           a.click();
           a.remove();
 
+
           this.emailprop = {
             Subject: this.emailForm.value['subject'],
-            EmailBody: this.emailForm.value['body']
+            EmailBody: this.emailForm.value['body'],
+            StartDate: this.adminService.returnFormatedDate(this.startDate),
+            EndDate: this.adminService.returnFormatedDate(this.endDate),
+            Status: this.statusFilter < 0 ? null:this.statusFilter
           };
   
+          console.log("Subject : " + this.emailprop.Subject);
+          console.log("EmailBody : " + this.emailprop.EmailBody);
+          console.log("StartDate : " + this.emailprop.StartDate);
+          console.log("EndDate : " + this.emailprop.EndDate);
+          console.log("Status : " + this.emailprop.Status);
+
           this.adminService.sendmailToAllPatient(this.emailprop).subscribe(response => {
             if(response == true){
               //this.emailForm.reset();
